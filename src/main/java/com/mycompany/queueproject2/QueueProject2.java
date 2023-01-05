f8193956207b5640c64d991e5b50d9f225286350
Vladislav Kondratyev <Kondraty_946191@leanderisd.org>
2023-01-05 10:59:23 -0600

    Files

---
+++ src/main/java/com/mycompany/queueproject2/QueueProject2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.queueproject2;
+
+/**
+ *
+ * @author kondraty_946191
+ */
+public class QueueProject2 {
+
+    public static void main(String[] args) {
+        System.out.print("HI");
+        new MainWindow().setVisible(true);
+    }
+    
+    public void TestMd(){
+        new MainWindow().setVisible(true);
+    }
+}
